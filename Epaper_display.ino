d527636484faca6d0928d774e5195df44000489a
Marks Lavrenovs <marks.lavrenovs@gmail.com>
2026-07-19 16:39:12 +0300

    Add files via upload

---
+++ Epaper_display.ino
@@ -0,0 +1,203 @@
+#include <GxEPD2_BW.h>
+#include <GxEPD2_3C.h>
+#include <GxEPD2_4C.h>
+#include <GxEPD2_7C.h>
+#include <Fonts/FreeMonoBold9pt7b.h>
+#include <Fonts/pixeldeb8pt7b.h>
+#include <Fonts/pixeldeb28pt7b.h>
+#include <Fonts/pixeldeb14pt7b.h>
+#include <Fonts/pixeldeb__1_40pt7b.h>
+#define CS_PIN (17)
+#define BUSY_PIN (14)
+#define RES_PIN (15)
+#define DC_PIN (16)
+
+int updatecount = 0;
+
+uint16_t leftgraphx = 22;
+uint16_t rightgraphx = 233;
+uint16_t graphY = 20;
+uint16_t graphH = 147;
+uint16_t graphW = 147;
+uint16_t newcputemp = 78;
+uint16_t newgputemp = 59;
+String newtime = "00:00";
+String lastDisplayedTime = "";
+int cpugrapharray[49] = {0};
+int gpugrapharray[49] = {0};
+uint16_t lastcputemp = 0;
+uint16_t lastgputemp = 0;
+
+
+GxEPD2_BW<GxEPD2_420_GDEY042T81, GxEPD2_420_GDEY042T81::HEIGHT> display(GxEPD2_420_GDEY042T81(CS_PIN, DC_PIN, RES_PIN, BUSY_PIN));
+void setup() {
+  
+  //display.init(115200); // default 10ms reset pulse, e.g. for bare panels with DESPI-C02
+  Serial.begin(115200);
+  display.init(115200, true, 2, false); // USE THIS for Waveshare boards with "clever" reset circuit, 2ms reset pulse
+  display.setFullWindow();
+  display.setRotation(180);
+  display.firstPage();
+  do{
+    routineRefresh();
+    display.fillScreen(GxEPD_WHITE);
+    basic_layout(); 
+  } while (display.nextPage());
+}
+
+
+void basic_layout() {
+    display.setTextColor(GxEPD_BLACK);
+    display.fillRect(19, 20, 3, 150, GxEPD_BLACK);
+    display.fillRect(230, 20, 3, 150, GxEPD_BLACK);
+    display.fillRect(19, 167, 150, 3, GxEPD_BLACK);
+    display.fillRect(230, 167, 150, 3, GxEPD_BLACK);
+    display.setFont(&pixeldeb8pt7b);
+    display.setCursor(44, 190);
+    display.print("CPU:");
+    display.setCursor(255, 190);
+    display.print("GPU:");
+    //after this there is code for bottom sections, for them to be stationary for now cuz I do not want to work on right now
+}
+
+void updatetime() {
+  if (newtime == lastDisplayedTime) return;  // skip if unchanged!
+  lastDisplayedTime = newtime;
+  
+  display.setPartialWindow(90, 220, 300, 80);
+  display.firstPage();
+  do {
+    display.fillRect(90, 220, 300, 80, GxEPD_WHITE);
+    display.setCursor(95, 280);
+    display.setFont(&pixeldeb__1_40pt7b);
+    display.print(newtime);
+  } while (display.nextPage());
+}
+
+void updategputemp() {
+  if (newgputemp == lastgputemp) return;
+  lastgputemp = newgputemp;
+  
+  display.setPartialWindow(299, 180, 60, 14);
+  display.firstPage();
+  char gpu[10];
+  sprintf(gpu, "%dC", newgputemp);
+  do {
+    display.fillRect(299, 180, 60, 14, GxEPD_WHITE);
+    display.setCursor(299, 190);
+    display.setFont(&pixeldeb8pt7b);
+    display.print(gpu);
+  } while (display.nextPage());
+}
+
+
+
+void updatecputemp() {
+    if (newcputemp == lastcputemp) return;
+    lastcputemp = newcputemp;
+    display.setPartialWindow(89, 180, 60, 14);
+    display.firstPage();
+    char cpu[10];
+    sprintf(cpu, "%dC", newcputemp);
+    do {
+      display.fillRect(89, 180, 60, 14, GxEPD_WHITE);
+      display.setCursor(89, 190);
+      display.setFont(&pixeldeb8pt7b);
+      display.print(cpu);
+    }
+    while (display.nextPage());
+}
+
+void cpugraph() {
+  display.setPartialWindow(24, 20, 147, 147);
+  display.firstPage();
+  for (int c = 0; c < 48; c++) {
+    cpugrapharray[c] = cpugrapharray[c + 1];
+  }
+  cpugrapharray[48] = newcputemp;
+  do{
+    display.fillRect(24, 20, 147, 147, GxEPD_WHITE);
+    for (int n = 0; n < 49; n++) {
+    int cpudotX = 22 + 3 * n;
+    int cpuY = cpugrapharray[n] * graphH / 98;
+    int cpudotY = graphY + graphH - cpuY;
+    display.fillRect(cpudotX, cpudotY, 3, 3, GxEPD_BLACK);
+  }
+  } while (display.nextPage());
+}
+void updatecpu() {
+  updatecputemp();
+  cpugraph();
+}
+
+void gpugraph() {
+  display.setPartialWindow(233, 20, 147, 147);
+  display.firstPage();
+  for (int g = 0; g < 48; g++) {
+    gpugrapharray[g] = gpugrapharray[g + 1];
+  }
+  gpugrapharray[48] = newgputemp;
+  do{
+    display.fillRect(233, 20, 147, 147, GxEPD_WHITE);
+    for (int n = 0; n < 49; n++) {
+    int gpudotX = 233 + 3 * n;
+    int gpuY = gpugrapharray[n] * graphH / 98;
+    int gpudotY = graphY + graphH - gpuY;
+    display.fillRect(gpudotX, gpudotY, 3, 3, GxEPD_BLACK);
+  }
+  } while (display.nextPage());
+}
+
+void updategpu() {
+  updategputemp();
+  gpugraph();
+}
+
+void update() {
+  updatecpu();
+  updategpu();
+  updatetime();
+}
+
+void routineRefresh() {
+  display.setFullWindow();
+  
+  display.firstPage();
+  do {
+    display.fillScreen(GxEPD_BLACK);
+  } while (display.nextPage());
+  
+  display.firstPage();
+  do {
+    display.fillScreen(GxEPD_WHITE);
+  } while (display.nextPage());
+  
+  basic_layout(); 
+}
+
+void loop() {
+  if (Serial.available()) {
+    String data = Serial.readStringUntil('\n');
+    int commaindex = data.indexOf(',');
+    newcputemp = data.substring(data.indexOf(':') + 1, commaindex).toInt();
+    int secondcomma = data.lastIndexOf(',');
+    newgputemp = data.substring(data.indexOf(':', commaindex) + 1, secondcomma).toInt();
+    newtime = data.substring(data.indexOf(':', secondcomma) + 1);
+    updatecount++;
+    if (updatecount >= 700) {
+      routineRefresh();
+      updatecount = 0;
+    }
+    update();
+  }
+
+};
+
+          // just some notes
+    //display.drawRect(89, 180, 60, 14, GxEPD_BLACK); //for future me, to make this partial refresh coordinates
+    //display.setCursor(89, 190);
+    //display.print("50c");
+    //display.drawRect(299, 180, 60, 14, GxEPD_BLACK); //second partial refresh window
+    //display.drawRect(0, 200, 199, 99, GxEPD_BLACK); //Border of the left bottom section
+    //display.drawRect(200, 200, 199, 99, GxEPD_BLACK); //Border of the right bottom section
+    //display.drawRect(200,200, 199, 50, GxEPD_BLACK); //right bottom section divider
---
+++ README.md
@@ -0,0 +1,73 @@
+# Epaper temperature display
+
+This is a README for the project
+
+Im going to assume that you are smarter than me(me when I started the project I mean),  
+if you need more detailed info for the intalation process, read the Detailed_Guide.md
+
+### How it was created
+
+I saw a review of Valve's steam machine, and noticed that they have a  
+special cover, that displayed the systems temperature and other stuff.  
+I thought: "damn, thats a cool thing, lemme make it!"(exact wording of my thought btw).  
+I got the display, started coding the basic layout function, just to get a raw demo, of how  
+it will look like when finished.
+
+After that, I used my 9th grade math(I am now about to start highschool(as of 19th of July 2026))  
+to make a formula to draw the graph. After explaining the logic behind my thinking, I put the  
+.txt file into Claude (dont tell them about my age please:), and it created some syntax, which I then  
+polished(I can assure you 99% of the logic you see in the code is made by me, I used AI only for  
+syntax, since Im a newbie).
+
+After several attpempts, I have gotten a clean (well, not proffesianally clean, but working and  
+somewhat understandable) code. 
+
+After that came the 3d modeling, and thats where I am somewhat skilled. I designed a case for  
+the display I had, and after some resting for rethinking the choices I made in the design 3d printed it.
+
+After assembly came some finishing code touches, then came the setup on Task Scheduler, which was quite easy.
+
+
+### Parts used
+
+Raspberry Pi Pico
+
+WeAct Studio 4.2 inch display ("WeAct 4.2'' 4.2 Inch Epaper Module E-paper E-Ink EInk Display Screen SPI Black-White" on aliexpress)
+
+USB A to microUSB cable
+
+Some TheFillament PLA HS
+
+### How to set up
+
+If using the same parts as me:  
+
+Install all the needed libraries(they are mentioned in the python script).
+
+Connect the pins to the ones marked in teh code(not neccesarry for most of them, some can be changed in code ofc)
+
+3d print out the case with the .step file linked
+
+Glue the parts to one another in the spots they should go (it is assembled in the file, so you can use it as guidance).
+
+Install Libre Hardware Monitor and run the server
+
+Schedule the launch of LHM in the Task Scheduler on boot.
+
+Test if the script is working for you(I mean the update.pyw).
+
+If it does, schedule it in the Task Scheduler on boot, but with some delay(mine is set to 30 seconds, just for sure).
+
+If not, you might need to search for the right ID in the json file and change it in the script.
+
+Then plug in the cable, and upload the code to raspberry pi.
+
+After that, to test it all(without restearting your system) justrun the update file in cmd and watch the magic happen.
+
+
+### Features 
+Shows CPU temp in both numbers and a graph.
+
+Shows GPU temp in both numbers and a graph.
+
+Shows time.
